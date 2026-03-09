29405cff1ebf71c9dd7c8de87a88dfc9b7b0cda3
ursteiner <christoph.mueller.zw@googlemail.com>
2026-03-09 21:37:16 +0100

    basic tests for URL analyzers

---
+++ src/test/java/com/github/ursteiner/movietracker/component/AmazonUrlAnalyzerTest.java
@@ -0,0 +1,47 @@
+package com.github.ursteiner.movietracker.component;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AmazonUrlAnalyzerTest {
+
+    private AmazonUrlAnalyzer analyzer;
+
+    @BeforeEach
+    public void setUp() {
+        analyzer = new AmazonUrlAnalyzer();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidUrls")
+    void getMovieId_ShouldReturnMovieId_WhenUrlIsValid(String url, String movieId) {
+        assertThat(analyzer.getMovieId(url)).isEqualTo(movieId);
+    }
+    @ParameterizedTest
+    @MethodSource("provideInvalidUrls")
+    void getMovieId_ShouldReturnNull_WhenUrlIsValid(String url) {
+        assertThat(analyzer.getMovieId(url)).isNull();
+    }
+
+    private static Stream<Arguments> provideValidUrls() {
+        return Stream.of(
+                Arguments.of("https://www.amazon.de/gp/video/detail/ABC1234", "ABC1234"),
+                Arguments.of("https://www.amazon.de/gp/video/detail/1234567?trackId=123456", "1234567"),
+                Arguments.of("https://www.amazon.de/gp/video/detail/xyz1234/ref=321", "xyz1234")
+        );
+    }
+
+    private static Stream<String> provideInvalidUrls() {
+        return Stream.of(
+                "https://www.amazon.de/gp/video/abc12345678",
+                "https://www.amazon.de",
+                ""
+        );
+    }
+}
---
+++ src/test/java/com/github/ursteiner/movietracker/component/NetflixUrlAnalyzerTest.java
@@ -0,0 +1,46 @@
+package com.github.ursteiner.movietracker.component;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class NetflixUrlAnalyzerTest {
+
+    private NetflixUrlAnalyzer analyzer;
+
+    @BeforeEach
+    public void setUp() {
+        analyzer = new NetflixUrlAnalyzer();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideValidUrls")
+    void getMovieId_ShouldReturnMovieId_WhenUrlIsValid(String url, String movieId) {
+        assertThat(analyzer.getMovieId(url)).isEqualTo(movieId);
+    }
+    @ParameterizedTest
+    @MethodSource("provideInvalidUrls")
+    void getMovieId_ShouldReturnNull_WhenUrlIsValid(String url) {
+        assertThat(analyzer.getMovieId(url)).isNull();
+    }
+
+    private static Stream<Arguments> provideValidUrls() {
+        return Stream.of(
+                Arguments.of("https://www.netflix.com/watch/12345678", "12345678"),
+                Arguments.of("https://www.netflix.de/watch/1234567?trackId=123456", "1234567")
+        );
+    }
+
+    private static Stream<String> provideInvalidUrls() {
+        return Stream.of(
+                "https://www.netflix.com/watch/abc12345678",
+                "https://www.netflix.com/watch/",
+                "https://www.netflix.com",
+                ""
+        );
+    }
+}
